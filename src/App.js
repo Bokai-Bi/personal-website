92b22788c61d50b86c5c85baa014901751a23ff4
bokai_bi <bokai.bi0@gmail.com>
2023-07-29 16:50:00 -0400

    Created basic framework for frontend

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavigationBar from './components/navbar.component';
+import Home from './components/home.component';
+import Playground from './components/playground.component';
+import NotFound from './components/notfound.component';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <NavigationBar />
+      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/playground' element={<Playground />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
