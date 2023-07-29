32533062564179aacd1e9ba571dec9d7d59c18e7
bokai_bi <bokai.bi0@gmail.com>
2023-07-29 18:44:13 -0400

    Added settings file for API Endpoint

---
+++ src/settings.js
@@ -0,0 +1,10 @@
+const LOCAL_IP = "129.213.102.19";
+const LOCAL_PORT = "80";
+//const BACKEND_IP = "129.213.102.19";
+const BACKEND_IP = "10.38.155.115";
+const BACKEND_PORT = "5140";
+
+module.exports.LOCAL_IP = LOCAL_IP;
+module.exports.LOCAL_PORT = LOCAL_PORT;
+module.exports.BACKEND_IP = BACKEND_IP;
+module.exports.BACKEND_PORT = BACKEND_PORT;
